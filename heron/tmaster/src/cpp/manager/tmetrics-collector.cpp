2e4b303dc1eb48877ae28f3844296d86713e5c83
Faria Kalim <faria.kalim@gmail.com>
2018-03-15 23:26:18 -0500

    committing more logs

--- heron/tmaster/src/cpp/manager/tmetrics-collector.cpp
+++ heron/tmaster/src/cpp/manager/tmetrics-collector.cpp
@@ -389,7 +389,7 @@ void TMetricsCollector::InstanceMetrics::GetMetrics(const MetricRequest& request
   for (sp_int32 i = 0; i < request.metric_size(); ++i) {
     const sp_string& id = request.metric(i);
     if (metrics_.find(id) != metrics_.end()) {
-      LOG(INFO) << "Metrics ID: " << id << " instance metrics "  + instance_id_ + " instance id" ;
+      LOG(INFO) << "Metrics ID: " << id << " instance metrics "  + instance_id_ + " instance id";
       metrics_[id]->GetMetrics(request.minutely(), start_time, end_time, m->add_metric());
     }
   }
